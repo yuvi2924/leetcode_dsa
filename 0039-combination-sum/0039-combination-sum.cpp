f2bd116f809995991f7b396a5117a696d2bb03ea
yuvi2924 <yuvisharma17654@gmail.com>
2026-09-23 19:48:56 +0530

    Time: 1 ms (62.44%), Space: 18.2 MB (19.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-void combination(vector<vector<int>> & ans,vector<int> v,vector<int> &candidates,int target,int idx){
-        if(target==0){
-       ans.push_back(v);
-       return;
-        }
-        if(target<0) return;
-        for(int i=idx;i<candidates.size();i++){
-            v.push_back(candidates[i]);
-            combination(ans,v,candidates,target-candidates[i],i);
-            v.pop_back();
-           }
-        }
+void fun(vector<int>& arr,int n,int idx,vector<int>&diary,int sum,vector<vector<int>>&res,int tar){
+  //base case 
+    if(idx==n){
+        if(sum==tar)
+            res.push_back(diary);
+        return;
+    }
+//choice 1 no change
+ fun(arr,n,idx+1,diary,sum,res,tar);
+ //choice 2
+ if(arr[idx]+sum<=tar){
+    diary.push_back(arr[idx]);
+    sum+=arr[idx];
+ fun(arr,n,idx,diary,sum,res,tar);
+    diary.pop_back();
+        sum-=arr[idx];
+ }
+     return;
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> v;
-        combination(ans,v, candidates,target,0);
-        return ans;
+        int n=candidates.size();
+        vector<vector<int>>res;
+        vector<int>diary;
+        fun(candidates,n,0,diary,0,res,target);
+        return res;
     }
 };
