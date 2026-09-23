f9413d2892729c93cf175e87deaac39e66d4c854
yuvi2924 <yuvisharma17654@gmail.com>
2026-09-23 09:30:35 +0530

    Time: 0 ms (100%), Space: 10.4 MB (8.64%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        //reverse the matrix
+        reverse(matrix.begin(),matrix.end());
+        //Transpose 
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
